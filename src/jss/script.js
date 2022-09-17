e43f23ae9482a89b540158a5a039f353fb4fff35
Roman <romawasserman@gmail.com>
2022-09-17 23:02:41 +0300

    sliders

---
+++ src/jss/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../images/chevron left solid.png"</button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../images/chevron right solid.png"</button>',
+
+      }    
+);
+  });
